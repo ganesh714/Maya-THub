dbdc130ffe447d637d34c6da93ce5a534a43e479
ganesh714 <138519420+ganesh714@users.noreply.github.com>
2024-12-31 17:54:48 +0530

    Add files via upload

---
+++ python/self_dividing_number.py
@@ -0,0 +1,14 @@
+def is_SDN(n):
+    for digit in str(n):
+        dig = int(digit)
+        if dig != 0 and n%dig != 0 :
+            print(digit)
+            return False
+    return True
+
+a = int(input()) 
+b = int(input()) 
+
+for i in range(a,b+1):
+    if is_SDN(i) == True:
+        print(i) 
