9fc3a8bfd936f234492892bd028c5029a487bbff
ganesh714 <138519420+ganesh714@users.noreply.github.com>
2025-01-05 21:47:15 +0530

    Create required_points.java

---
+++ java/required_points.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+public class Rp{
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+        int a = scanner.nextInt();
+        int b = scanner.nextInt();
+
+        System.out.println(b/a);    
+    }
+}
