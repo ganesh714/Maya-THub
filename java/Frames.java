70abcd28acc360dafe18de5035f6c024a0ce1cce
ganesh714 <138519420+ganesh714@users.noreply.github.com>
2025-01-05 20:54:01 +0530

    Update Frames.java

--- java/Frames.java
+++ java/Frames.java
@@ -1,10 +1,10 @@
-import java.until.Scanner;
+import java.util.Scanner;
 public class Frames{
     public static void main(String[] args){
         Scanner scanner = new Scanner(System.in);
-        int n = Scanner.nextInt();
-        int m = Scanner.nextInt();
-        int x = Scanner.nextInt();
+        int n = scanner.nextInt();
+        int m = scanner.nextInt();
+        int x = scanner.nextInt();
 
         c = x * 2*(m+n);
         System.out.println(c);
