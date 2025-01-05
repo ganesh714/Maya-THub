a70c6ad2c32096951ec9ad7ce90a1f213dc5dd57
ganesh714 <138519420+ganesh714@users.noreply.github.com>
2025-01-05 21:06:52 +0530

    Create area_and_perimeter_of_square.java

---
+++ java/area_and_perimeter_of_square.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+public class Frames{
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+        int s = scanner.nextInt();
+
+        System.out.printf( (s*s) + " " + (4*s));    
+    }
+}
