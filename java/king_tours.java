077307e7c27f8f44759bed5a26c5f8e180fe3f94
ganesh714 <138519420+ganesh714@users.noreply.github.com>
2025-01-05 20:55:03 +0530

    Create king_tours.java

---
+++ java/king_tours.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+public class Frames{
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        int m = scanner.nextInt();
+
+        System.out.println((n*5) + (m*7));
+    }
+}
