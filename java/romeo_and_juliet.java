778b6882f64273841650121018cd2f70b60ba092
ganesh714 <138519420+ganesh714@users.noreply.github.com>
2025-01-06 06:44:13 +0530

    Create romeo_and_juliet.java

---
+++ java/romeo_and_juliet.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class Class{
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+        int x = scanner.nextInt();
+        int y = scanner.nextInt();
+        int z = scanner.nextInt();
+
+        System.out.println(((x*5) + (y*10))/z);
+
+    }
+}
