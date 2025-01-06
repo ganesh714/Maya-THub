a288df08576968e1b6e30fb6a18d6fc74e7a1f14
ganesh714 <138519420+ganesh714@users.noreply.github.com>
2025-01-06 06:06:26 +0530

    Create inches_to_centimeters.java

---
+++ java/inches_to_centimeters.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+public class IC{
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+
+        System.out.printf("%.2f",n*2.54);
+    }
+}
