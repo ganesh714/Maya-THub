fedf93320bb0a964ff2b81e222ab4239918d85a3
ganesh714 <138519420+ganesh714@users.noreply.github.com>
2025-01-18 19:46:40 +0530

    Create area_of_a_circle.java

---
+++ java/area_of_a_circle.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class division {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int r = scanner.nextInt();
+        System.out.println(3.14 * (r*r));
+
+    }
+}
