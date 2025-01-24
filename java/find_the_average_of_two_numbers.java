ecd2bca97ae1972cd6b214258bfed2c95be6c593
ganesh714 <138519420+ganesh714@users.noreply.github.com>
2025-01-24 18:06:06 +0530

    Create find_the_average_of_two_numbers.java

---
+++ java/find_the_average_of_two_numbers.java
@@ -0,0 +1,14 @@
+//link --> https://maya.technicalhub.io/owl-program-details/669b78f584140b42f82a2f8a
+
+import java.util.Scanner;
+
+public class AVT {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        double a = scanner.nextDouble();
+        double b = scanner.nextDouble();
+        System.out.printf("%.4f",(a+b)/2f);
+
+    }
+}
