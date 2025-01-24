89c297df34b4288be38119f0c80a52bc5c504898
ganesh714 <138519420+ganesh714@users.noreply.github.com>
2025-01-24 18:32:17 +0530

    Create find_ASCII_value_of_a_character.java

---
+++ java/find_ASCII_value_of_a_character.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Maya {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        char a = scanner.next().charAt(0);
+        int asci = a;
+        System.out.printf("%d",asci);
+
+    }
+}
