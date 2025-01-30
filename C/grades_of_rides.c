9c2393a01ddec5002ad481cb5969b0dba3b0262c
ganesh714 <138519420+ganesh714@users.noreply.github.com>
2025-01-30 22:49:29 +0530

    Create grades_of_rides.c

---
+++ C/grades_of_rides.c
@@ -0,0 +1,25 @@
+ #include<stdio.h>
+ int main(){
+    int hf,sf,sp;
+
+    scanf("%d%d%d",&hf,&sf,&sp);
+
+    if(hf > 50 && sf > 60 && sp > 100 ){
+        printf("10");
+    }
+    else if (hf > 50 && sf > 60){
+        printf("9");
+    }
+    else if (sf > 60 && sp > 100){
+        printf("8");
+    }
+    else if (hf > 50 && sp > 100){
+        printf("7");
+    }
+    else if (hf > 50 || sf > 60 || sp > 100){
+        printf("6");
+    }
+    else{
+        printf("5");
+    }
+ }
