3db87177b529be37a1515dd7679dbe0c955f696b
ganesh714 <138519420+ganesh714@users.noreply.github.com>
2025-02-01 18:13:40 +0530

    Create donation_rewards.c

---
+++ C/donation_rewards.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main(){
+    int n;
+    scanf("%d",&n);
+    if (n <= 3)
+        printf("BRONZE");
+    else if ( n <= 6)
+        printf("SILVER");
+    else 
+        printf("GOLD");
+}
