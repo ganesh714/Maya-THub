1ab6be679c582111a59b15b37b1cda50ebdefc7d
ganesh714 <138519420+ganesh714@users.noreply.github.com>
2025-02-28 08:45:28 +0530

    Create categorize.java

---
+++ java/categorize.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class CF{
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int count = 0;
+        float h = scanner.nextFloat();
+        if (h < 150) {
+            System.out.println("Person is Dwarf.");
+        }
+        else if (h <= 165) {
+            System.out.println("Person is average heighted.");
+        }
+        else if (h <= 195) {
+            System.out.println("Person is taller.");
+        }
+        else{
+            System.out.println("Abnormal height.");
+        }
+    }
+}
