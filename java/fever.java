fa0cdbaf219deb9a36c6640b6ff9f4b1faf68419
ganesh714 <138519420+ganesh714@users.noreply.github.com>
2025-02-28 08:57:04 +0530

    Create fever.java

---
+++ java/fever.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Maya{
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int f = scanner.nextInt();
+        if (f > 98) {
+            System.out.println("YES");
+        }
+        else{
+            System.out.println("NO");
+        }
+    }
+}
