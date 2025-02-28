6ef9fb237d7299ceae20dc7265e73f7931616cdc
ganesh714 <138519420+ganesh714@users.noreply.github.com>
2025-02-28 23:13:12 +0530

    Update swap_two_numbers.java

--- java/swap_two_numbers.java
+++ java/swap_two_numbers.java
@@ -1,12 +1,15 @@
 import java.util.Scanner;
-public class Class{
-    public static void main(String[] args){
+
+public class CF{
+    public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int a = scanner.nextInt();
         int b = scanner.nextInt();
         
-        a = (a+b) - b;
-        b = (a+b) - b;
+        a = a+b;
+        b = a - b;
+        a = a - b;
         System.out.println(a + " " +  b);
     }
 }
+
