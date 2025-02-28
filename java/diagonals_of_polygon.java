426b8c052b118d28c1c3ba8408fdfc31726cc8e1
ganesh714 <138519420+ganesh714@users.noreply.github.com>
2025-02-28 09:06:07 +0530

    Create diagonals_of_polygon.java

---
+++ java/diagonals_of_polygon.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+public class maya{
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+
+        System.out.println((n)*(n-3)/2);
+    }
+}
