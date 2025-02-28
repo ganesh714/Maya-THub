fbd673cc2e61941d09e87d1d1d20bbd72494dab6
ganesh714 <138519420+ganesh714@users.noreply.github.com>
2025-02-28 09:04:26 +0530

    Create donation.java

---
+++ java/donation.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class Maya{
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int x = scanner.nextInt();
+        int y = scanner.nextInt();
+        System.out.println(y-x);
+    }
+}
