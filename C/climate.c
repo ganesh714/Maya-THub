21795dd7295b90a5ec0e878053576717524488a9
ganesh714 <138519420+ganesh714@users.noreply.github.com>
2025-03-02 23:14:22 +0530

    Create climate.c

---
+++ C/climate.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main(){
+    int t;
+    scanf("%d",&t);
+    if (t <= 20)
+    {
+        printf("COLD");
+    }
+    else{
+        printf("HOT");
+    }
+    
+}
+
+
+
+
+
+
+
+
+//---->   https://maya.technicalhub.io/owl-program-details/6688c350e467bfe336f7c592
