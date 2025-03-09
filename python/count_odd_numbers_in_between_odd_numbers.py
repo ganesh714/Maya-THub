905bea44274b26675d060c54671f18fc306ee7f1
ganesh714 <138519420+ganesh714@users.noreply.github.com>
2025-03-09 13:04:49 +0530

    Create count_odd_numbers_in_between_odd_numbers.py

---
+++ python/count_odd_numbers_in_between_odd_numbers.py
@@ -0,0 +1,11 @@
+def count_between_even(arr,n):
+    count = 0
+    for i in range(1, n - 1):
+        if (arr[i - 1] % 2 != 0 and arr[i + 1] % 2 != 0 and arr[i] % 2 != 0) :
+            count += 1
+    return count
+
+n = int(input())
+arr = list(map(int, input().split()))
+
+print(count_between_even(arr,n))
